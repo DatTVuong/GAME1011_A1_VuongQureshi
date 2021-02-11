9850746e47450a36a4ec56db43ff9da90797d5cc
Dat <Davidvuong67@gmail.com>
2021-02-10 23:02:19 -0500

    Actual code shit
    beginning of code

---
+++ GAME1011_A1_VuongQureshi/GAME1011_A1_VuongQureshi/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "                               [-----[Welcome to the survey]-----]" << std::endl;
+	std::cout << "This program gathers data about the entertainment consumption habits between college students." << std::endl;
+}
