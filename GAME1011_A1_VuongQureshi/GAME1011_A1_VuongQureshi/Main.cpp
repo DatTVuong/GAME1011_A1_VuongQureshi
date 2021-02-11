5ba5fdf3938d8518a903357a4dc179d3d87a7cb2
Dat <Davidvuong67@gmail.com>
2021-02-10 23:33:35 -0500

    Person Class
    Created and worked on person class

--- GAME1011_A1_VuongQureshi/GAME1011_A1_VuongQureshi/Main.cpp
+++ GAME1011_A1_VuongQureshi/GAME1011_A1_VuongQureshi/Main.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
-	std::cout << "                               [-----[Welcome to the survey]-----]" << std::endl;
-	std::cout << "This program gathers data about the entertainment consumption habits between college students." << std::endl;
+	int TotalParticipants; //Total number of Participants
+
+	//Beginning of the program
+	std::cout << "[-----[Welcome to the survey]-----]" << std::endl;                                                               //Welcomes the user to the application
+	std::cout << "This program gathers data about the entertainment consumption habits between college students." << std::endl;    //Intro. Message
 }
--- GAME1011_A1_VuongQureshi/GAME1011_A1_VuongQureshi/Person.h
+++ GAME1011_A1_VuongQureshi/GAME1011_A1_VuongQureshi/Person.h
@@ -1,1 +1,32 @@
 #pragma once
+#ifndef __PERSON__
+#define __PERSON__
+#include <string>
+
+class Person
+{
+private:                      //Private Variables
+	int age;                  //Age
+	std::string fullName;     //Full Name 
+
+public:
+	//Constructor for age
+	Person (int age){
+		this->age = age;
+	}
+	
+	//Constructor for name
+	Person (std::string name){
+		this->Name = name;
+	}
+
+	//Setters
+	void setAge(int age);
+	void setName(std::string name);
+
+	//Getters
+	int getAge();
+	std::string getName();
+
+};
+#endif
